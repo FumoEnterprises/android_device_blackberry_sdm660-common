48db4793bfaf1998a54988ba004ac592da873fe5
tim-ecoder <timafyozzi@yandex.ru>
2025-11-11 09:56:27 +0330

    Little bit logging kmsg and logcat fixed

--- init/loggy.sh
+++ init/loggy.sh
@@ -1,4 +1,6 @@
 #!/system/bin/sh
 date=`date +%F_%H-%M-%S`
-logcat -b all -f  /cache/logcat_${_date}.txt &
-dmesg -w > /cache/kmsg_${_date}.txt
+rm /cache/logcat.txt
+rm /cache/kmsg.txt
+logcat -b all -f  /cache/logcat.txt &
+dmesg -w > /cache/kmsg.txt
