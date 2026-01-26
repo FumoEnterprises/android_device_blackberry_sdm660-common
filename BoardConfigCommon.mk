b4e2261650156b8b603e335630d8ba6af76bea44
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-01-26 21:10:33 +0100

    sdm660: BoardConfig: Optimize for A73 cores

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,18 +18,18 @@ BOARD_VENDOR := blackberry
 
 COMMON_PATH := device/blackberry/sdm660-common
 
-# Platform
+# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a73
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT := cortex-a73
 
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := sdm660
