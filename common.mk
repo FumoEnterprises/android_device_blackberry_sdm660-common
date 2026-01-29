3a1c0259a45244726d4048a4cce19a855fb23833
Johnathan Cintron <squash@shadowfall.me>
2026-01-29 04:40:43 -0500

    Yeet RCS and add vendor IMS prop

--- common.mk
+++ common.mk
@@ -319,10 +319,10 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
 
 # RCS - not supported by modem and causes log spam. TODO
-PRODUCT_PACKAGES += \
-    com.android.ims.rcsmanager \
-    PresencePolling \
-    RcsService
+#PRODUCT_PACKAGES += \
+#    com.android.ims.rcsmanager \
+#    PresencePolling \
+#    RcsService
 
 # RIL
 PRODUCT_PACKAGES += \
