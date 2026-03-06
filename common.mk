2b29430b28777c26f36e05b8297f0b2e1b656b26
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-03-06 19:22:42 +0100

    sdm660-common: Merge {rootdir, libinit} -> init
    Change-Id: Id81e6fca3f754b91cb3c65c286ce55485a301e64
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

--- common.mk
+++ common.mk
@@ -346,6 +346,7 @@ PRODUCT_PACKAGES += \
     init.blackberry.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
+    init.qcom.recovery.rc \
     init.target.rc \
     ueventd.qcom.rc
 
@@ -356,7 +357,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
-    init.qti.qseecomd.sh
 
 # RenderScript
 PRODUCT_PACKAGES += \
@@ -385,7 +385,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr
+    hardware/lineage/interfaces/power-libperfmgr \
+    vendor/qcom/opensource/usb/etc
 
 # Telephony-ext
 PRODUCT_PACKAGES += \
